ee756986b1dd1f26627893f9e657adb309c37d3b
Neil <nguyentrungnghia320@gmail.com>
2021-09-22 16:28:33 +0700

    adapted new limbo contract

--- tasks/configuration.ts
+++ tasks/configuration.ts
@@ -1,11 +1,11 @@
-import { task } from "hardhat/config";
-import { FlashGovernanceArbiter__factory, Limbo__factory, UniswapHelper__factory } from "../typechain";
-import * as Addresses from "../addresses.json";
+// import { task } from "hardhat/config";
+// import { FlashGovernanceArbiter__factory, Limbo__factory, UniswapHelper__factory } from "../typechain";
+// import * as Addresses from "../addresses.json";
 
-task("end-config", "End configuration").setAction(async (args, hre) => {
-  const signer = (await hre.ethers.getSigners())[0];
-  const fga = FlashGovernanceArbiter__factory.connect(Addresses.FlashGovernanceArbiter, signer);
-  const limbo = Limbo__factory.connect(Addresses.Limbo, signer);
-  await fga.endConfiguration();
-  await limbo.endConfiguration();
-});
+// task("end-config", "End configuration").setAction(async (args, hre) => {
+//   const signer = (await hre.ethers.getSigners())[0];
+//   const fga = FlashGovernanceArbiter__factory.connect(Addresses.FlashGovernanceArbiter, signer);
+//   const limbo = Limbo__factory.connect(Addresses.Limbo, signer);
+//   await fga.endConfiguration();
+//   await limbo.endConfiguration();
+// });
