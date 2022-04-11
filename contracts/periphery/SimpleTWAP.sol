f263cbd9cd35f8ebbf378ce582a3644616d43b72
Justin Goro <justingoro@protonmail.com>
2022-04-11 02:27:12 +0200

    Oracle compiling not tested

--- contracts/periphery/SimpleTWAP.sol
+++ contracts/periphery/SimpleTWAP.sol
@@ -1,71 +1,116 @@
-// pragma solidity =0.6.6;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+import "./UniswapV2/interfaces/IUniswapV2Factory.sol";
+import "../DAO/Governable.sol";
 
-// import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
-// import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
-// import '@uniswap/lib/contracts/libraries/FixedPoint.sol';
+import "./UniswapV2/interfaces/IUniswapV2Pair.sol";
+import "./UniswapV2/lib/FixedPoint.sol";
 
-// import '../libraries/UniswapV2OracleLibrary.sol';
-// import '../libraries/UniswapV2Library.sol';
+import "./UniswapV2/libraries/UniswapV2OracleLibrary.sol";
+import "./UniswapV2/libraries/UniswapV2Library.sol";
 
-// /**
-//  *@title SoulReader
-//  * @author Justin Goro
-//  * @notice Coneptually similar to the Router contracts in Uniswap, this is a helper contract for reading Limbo data in a manner more friendly to a UI
-//  * @dev passing the limbo contract address in allows the SoulReader to remain stateless and also allows front end devs to perform comparisons and experiments in real time.
-//  */
-// contract ExampleOracleSimple {
-//     using FixedPoint for *;
+/**
+ *@title SoulReader
+ * @author Justin Goro
+ * @notice There are two reasons to gather reliable prices in Limbo: calculating Fate from relative EYE containing LP prices and pricing Flan during a migration to Behodler.
+ * Neither requires up to date accuracy but must simply be resitant to tampering.
+ * @dev This contract is based on the UniswapV2 ExampleOracleSimple. Originally, a simpler version of an Oracle was created but a tension emerged between griefing and security that only resolved in a hell of complication.
+ */
+contract SimpleTWAP is Governable {
+  using FixedPoint for *;
+    using FixedPoint for FixedPoint.uq112x112;
+  uint256 public constant PERIOD = 24 hours;
+  IUniswapV2Factory public factory;
+  struct PairMeasurement {
+    uint256 price0CumulativeLast;
+    uint256 price1CumulativeLast;
+    uint32 blockTimestampLast;
+    FixedPoint.uq112x112 price0Average;
+    FixedPoint.uq112x112 price1Average;
+    uint256 period;
+  }
+  mapping(address => PairMeasurement) public pairMeasurements;
 
-//     uint public constant PERIOD = 24 hours;
+  constructor(address V2factory, address limboDAO) Governable(limboDAO) {
+    factory = IUniswapV2Factory(V2factory);
+  }
 
-//     IUniswapV2Pair immutable pair;
-//     address public immutable token0;
-//     address public immutable token1;
+  function RegisterPair(address pairAddress, uint period) public onlySuccessfulProposal {
+    IUniswapV2Pair pair = IUniswapV2Pair(pairAddress);
+    uint256 price0CumulativeLast = pair.price0CumulativeLast(); // fetch the current accumulated price value (1 / 0)
+    uint256 price1CumulativeLast = pair.price1CumulativeLast(); // fetch the current accumulated price value (0 / 1)
+    uint112 reserve0;
+    uint112 reserve1;
+    uint32 blockTimestampLast;
+    (reserve0, reserve1, blockTimestampLast) = pair.getReserves();
+    require(reserve0 != 0 && reserve1 != 0, "ORACLE: NO_RESERVES"); // ensure that there's liquidity in the pair
+    pairMeasurements[pairAddress] = PairMeasurement({
+      price0CumulativeLast: price0CumulativeLast,
+      price1CumulativeLast: price1CumulativeLast,
+      blockTimestampLast: blockTimestampLast,
+        price0Average: FixedPoint.uq112x112(0),
+        price1Average: FixedPoint.uq112x112(0),
+        period: period * (1 hours)
+    });
+  }
 
-//     uint    public price0CumulativeLast;
-//     uint    public price1CumulativeLast;
-//     uint32  public blockTimestampLast;
-//     FixedPoint.uq112x112 public price0Average;
-//     FixedPoint.uq112x112 public price1Average;
+    function isPair(address tokenA, address tokenB) private view returns (bool){
+        return factory.getPair(tokenA, tokenB)!=address(0);
+    }
 
-//     constructor(address factory, address tokenA, address tokenB) public {
-//         IUniswapV2Pair _pair = IUniswapV2Pair(UniswapV2Library.pairFor(factory, tokenA, tokenB));
-//         pair = _pair;
-//         token0 = _pair.token0();
-//         token1 = _pair.token1();
-//         price0CumulativeLast = _pair.price0CumulativeLast(); // fetch the current accumulated price value (1 / 0)
-//         price1CumulativeLast = _pair.price1CumulativeLast(); // fetch the current accumulated price value (0 / 1)
-//         uint112 reserve0;
-//         uint112 reserve1;
-//         (reserve0, reserve1, blockTimestampLast) = _pair.getReserves();
-//         require(reserve0 != 0 && reserve1 != 0, 'ExampleOracleSimple: NO_RESERVES'); // ensure that there's liquidity in the pair
-//     }
+  function update(address token0, address token1) external {
+    require(isPair(token0, token1), "ORACLE: PAIR_NOT_FOUND");
+    address pair = factory.getPair(token0, token1);
+    _update(pair);
+  }
 
-//     function update() external {
-//         (uint price0Cumulative, uint price1Cumulative, uint32 blockTimestamp) =
-//             UniswapV2OracleLibrary.currentCumulativePrices(address(pair));
-//         uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
+  function update(address pair) external {
+    _update(pair);
+  }
 
-//         // ensure that at least one full period has passed since the last update
-//         require(timeElapsed >= PERIOD, 'ExampleOracleSimple: PERIOD_NOT_ELAPSED');
+  function _update(address _pair) private {
+    (uint256 price0Cumulative, uint256 price1Cumulative, uint32 blockTimestamp) = UniswapV2OracleLibrary
+      .currentCumulativePrices(_pair);
+    PairMeasurement memory measurement = pairMeasurements[_pair];
 
-//         // overflow is desired, casting never truncates
-//         // cumulative price is in (uq112x112 price * seconds) units so we simply wrap it after division by time elapsed
-//         price0Average = FixedPoint.uq112x112(uint224((price0Cumulative - price0CumulativeLast) / timeElapsed));
-//         price1Average = FixedPoint.uq112x112(uint224((price1Cumulative - price1CumulativeLast) / timeElapsed));
+    uint32 timeElapsed;
+    unchecked {
+      timeElapsed = blockTimestamp - measurement.blockTimestampLast; // overflow is desired
+    }
+    // ensure that at least one full period has passed since the last update
+    require(timeElapsed >= PERIOD, "ORACLE: PERIOD_NOT_ELAPSED");
 
-//         price0CumulativeLast = price0Cumulative;
-//         price1CumulativeLast = price1Cumulative;
-//         blockTimestampLast = blockTimestamp;
-//     }
+    // overflow is desired, casting never truncates
+    // cumulative price is in (uq112x112 price * seconds) units so we simply wrap it after division by time elapsed
+    measurement.price0Average = FixedPoint.uq112x112(
+      uint224((price0Cumulative - measurement.price0CumulativeLast) / timeElapsed)
+    );
+    measurement.price1Average = FixedPoint.uq112x112(
+      uint224((price1Cumulative - measurement.price1CumulativeLast) / timeElapsed)
+    );
 
-//     // note this will always return 0 before update has been called successfully for the first time.
-//     function consult(address token, uint amountIn) external view returns (uint amountOut) {
-//         if (token == token0) {
-//             amountOut = price0Average.mul(amountIn).decode144();
-//         } else {
-//             require(token == token1, 'ExampleOracleSimple: INVALID_TOKEN');
-//             amountOut = price1Average.mul(amountIn).decode144();
-//         }
-//     }
-// }
+    measurement.price0CumulativeLast = price0Cumulative;
+    measurement.price1CumulativeLast = price1Cumulative;
+    measurement.blockTimestampLast = blockTimestamp;
+    pairMeasurements[_pair] = measurement;
+  }
+
+  function consult(
+    address pricedToken,
+    address referenceToken,
+    uint256 amountIn
+  ) external view returns (uint256 amountOut) {
+    require(isPair(pricedToken, referenceToken), "ORACLE: PAIR_NOT_FOUND");
+    IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(pricedToken, referenceToken));
+    PairMeasurement memory measurement = pairMeasurements[address(pair)];
+
+    if (pricedToken == pair.token0()) {
+      amountOut = (measurement.price0Average.decode() * amountIn);
+    } else {
+      require(pricedToken == pair.token1(), "ORACLE: INVALID_TOKEN");
+      amountOut = measurement.price1Average.decode() *amountIn;
+    }
+
+    require(amountOut > 0, "ORACLE: UPDATE FIRST");
+  }
+}
