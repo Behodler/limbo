26c58e4a9c9378163ced87c82fe8bf7413f0d889
Justin Goro <justingoro@protonmail.com>
2022-08-17 08:20:24 +0200

    Can we mint it? Yes we Flan

--- contracts/FlanGenesis.sol
+++ contracts/FlanGenesis.sol
@@ -2,18 +2,45 @@
 pragma solidity 0.8.13;
 import "./facades/AngbandLike.sol";
 import "./facades/FlanLike.sol";
+import "./facades/BehodlerLike.sol";
+import "./periphery/UniswapV2/interfaces/IUniswapV2Factory.sol";
+import "./openzeppelin/Ownable.sol";
 // import "./facades/AddTokenToBehodlerPowerLike.sol";
 
 ///@author Justin Goro
-/**@notice to C4 auditors: this part of Limbo intersects significantly with MorgothDAO and is beyond the scope of the audit.
-* No prizes are offered for auditing MorgothDAO at this stage. While it's important that Flan be set up correctly, an incorrect setup
-* is easy to detect and costless to discard (ignoring gas costs) and so may be attempted multiple times until perfected. 
-* The migration to Behodler will require a surface level understanding of Morgoth the functionality employed by Morgoth has already withstood the test of multiple mainnet uses
+/**@notice beyond scope of current audit. We can try different strategies on testnet before we've settled with something we like before auditing.
+* No point in auditing now, only to throw it out on day 1.
 */
 ///@dev this contract combines multiple genesis operations into one transaction to protect against entereing into an invalid state
-contract FlanGenesis{
-    //TODO: full flan genesis with no rectangle of fairness. Use SCX bounty to pump up liquidity, perhaps on both Uni and sushi?
+contract FlanGenesis is Ownable{
     struct Dependencies {
-        uint something;
+        BehodlerLike behodler;
+        IUniswapV2Factory uniswapFactory;
+        IUniswapV2Factory sushiswapFactory;
     }    
+
+    Dependencies public dependencies;
+
+    function LetThereBeFlan(uint daiAVB, uint daiPerSCX) public onlyOwner{
+        //assert that the daiABV is the sams as on Behodler right now.
+        //assert that withdrawFindLiquidiy yields dai per SCX.
+
+        //call power to create and register flan awith lachesis and behodler. Parameterize power with daiAVB and scxPerDai. 
+        //We want to mint Flan quantity of daiAVB + scxMinted*DaiPerSCX + 500 and send 500 to pyroPower. 
+        //Send rest to this contract.Also send SCX to this contract.
+        //call pyro power to create pyro and mint 500 Pyro (to protect against redeem rate jigging).
+        //divide flan into 2 parts and mint PyroFlan. Divide remainin flan into 2 parts and pyro into 2 parts. Call these portions F0,F1, P0 and P1
+        //Unifactory.create pairs for Flan/SCX and PyroFlan/SCX. Same for Sushi factory.
+        //Let S4 = Scarcity balance divided by 4. Create LP tokens for F0/S4 on Uni,F1/S4 on Sushi,P0/S4 on Uni ,P1/S4 on Sushi.
+        //We assume the LP token balances are all of equal value. Dai value of an LP batch = F0x2. PriceOfLP = Dai_Value/balanceOFLP
+        // SeedQuantity = DaiAVB/PriceOfLP
+        //call power to register all 4 LPs with lachesis and seed Behodler with SeedQuantity. Return SCX generated to this contract.
+        //Flan to generate = (SCX_generated*daiPerSCX). Mint Flan_to_generate.  
+        //Let newly minted Flan by FN. Divide FN and newly generated SCX into 4. Restock each LP with FN and SCX
+        //Don't do anything with any remaining SCX, FLAN, PyroFlan or LP tokens. They are all considered burnt and liquidity is locked. 
+        //Until Limbo launches, the only way to get Flan will be to buy it from Uniswap, Sushiswap or behodler.
+
+        //Remember to end configuration of Limbo
+        //Also remember to get the cyclical dependencies correct. Whether to create Flan and Limbo first before running this Or to use this to create all of them.
+    }
 }
