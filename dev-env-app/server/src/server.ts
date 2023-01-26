9fd9b4a25631220d27df494d5e7b3700c5779796
michal.kloczko <michal.kloczko@tratif.com>
2023-01-26 13:01:34 +0100

    first working versin of starting dev env with an API call

--- dev-env-app/server/src/server.ts
+++ dev-env-app/server/src/server.ts
@@ -1,12 +1,26 @@
 import Fastify, { FastifyInstance } from 'fastify'
 import hre from 'hardhat'
+import '@nomiclabs/hardhat-ethers'
+import path from 'path'
+import { safeDeploy } from '../../../scripts/networks/orchestrate'
 
 export const fastify: FastifyInstance = Fastify({
-  logger: true,
+  logger: {
+    transport: {
+      target: "@fastify/one-line-logger",
+    },
+  },
 })
 
-fastify.get('/start-hardhat-node', async () => {
-  await hre.run('node', { noDeploy: true });
+function hreManager() {
+
+}
+
+fastify.get('/start', async () => {
+  // const { chainId } = await hre.ethers.provider.getNetwork()
+  const node = hre.run('run', { script: path.resolve(__dirname, '../../../scripts/start-dev-node.ts') })
+  // await safeDeploy(chainId, 2, 9)
+  return await node
 })
 
 fastify.listen({ port: 6667 })
--- scripts/start-dev-node.ts
+++ scripts/start-dev-node.ts
@@ -4,6 +4,7 @@ import {safeDeploy} from "./networks/orchestrate"
 import { takeSnapshot } from "@nomicfoundation/hardhat-network-helpers"
 import behodlerABI from "../ABIs/Behodler.json"
 import UniswapV2Pair from "../ABIs/UniswapV2Pair.json"
+
 async function deploy() {
     const { chainId } = await hre.ethers.provider.getNetwork();
     const deployment = await safeDeploy(chainId, 2, 9);
@@ -20,7 +21,7 @@ async function main() {
 
     console.info('snap', snap);
 
-    const wallet = new ethers.Wallet('pk', provider);
+    const wallet = new ethers.Wallet('cf4a9e84114acde4e307c37c27f91ea161516b83e70a8fe2096a97100beaedd9', provider);
 
     const ownerETHBalance = ethers.utils.formatEther(await provider.getBalance(wallet.address));
 
