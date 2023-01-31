91ed322497e1d476627cbbdfb0c4a6ad72f1be7e
michal.kloczko <michal.kloczko@tratif.com>
2023-01-31 01:24:49 +0100

    assure all app functions are running inside of the same Hardhat context, refactor

--- dev-env-app/server/src/server.ts
+++ dev-env-app/server/src/server.ts
@@ -1,12 +1,12 @@
+import hre, { ethers } from 'hardhat'
+import '@nomiclabs/hardhat-ethers'
+import { SnapshotRestorer, takeSnapshot } from '@nomicfoundation/hardhat-network-helpers'
 import Fastify, { FastifyInstance } from 'fastify'
 import path from 'path'
 import { ChildProcess, fork } from 'child_process'
+import fs from 'fs'
 import { HardhatContext } from 'hardhat/internal/context'
-import { SnapshotRestorer } from '@nomicfoundation/hardhat-network-helpers'
-
-import { ProcessMessage } from './types'
-
-HardhatContext.createHardhatContext()
+import { safeDeploy } from '../../../scripts/networks/orchestrate'
 
 const fastify: FastifyInstance = Fastify({
   logger: {
@@ -16,87 +16,153 @@ const fastify: FastifyInstance = Fastify({
   },
 })
 
-let devEnvChildProcess: ChildProcess | undefined
+let devEnvChildProcess: ChildProcess | undefined | {
+  killed: boolean
+  node: Promise<void>
+  deployedAddresses: any
+}
 let devEnvSnapshots: SnapshotRestorer[] = []
 
-fastify.post('/start', async () => {
-  if (!devEnvChildProcess || devEnvChildProcess?.killed) {
-    fastify.log.info('starting dev env')
-    devEnvChildProcess = fork(path.resolve(__dirname, './hre.ts'))
-    devEnvChildProcess.send({ type: 'start' })
+const paths = {
+  hardhatDeployedAddresses: path.resolve(
+    __dirname,
+    '../../../scripts/networks/addresses/hardhat.json',
+  ),
+}
 
-    devEnvChildProcess.on('message', (msg: ProcessMessage) => {
-      if (msg.type === 'snapshots') {
-        fastify.log.info('updating devEnvSnapshots')
-        devEnvSnapshots = [...msg.data]
-      }
-
-      if (msg.type === 'snapshotRestored') {
-        fastify.log.info(msg.data)
-      }
-
-      if (msg.type === 'snapshotRestoringFailed') {
-        fastify.log.info(msg.data)
-      }
-    })
-  } else {
-    fastify.log.info('dev env already started')
+async function runNodeAndDeploy() {
+  if (!HardhatContext.isCreated()) {
+    HardhatContext.createHardhatContext()
+    fastify.log.info('created hardhat context')
   }
-})
 
-fastify.post('/stop', async () => {
+  const node = hre.run('node', { noDeploy: true })
+  fastify.log.info('started hardhat node')
+  const { chainId } = await hre.ethers.provider.getNetwork()
+  fastify.log.info('deploying Behodler contracts')
+  const deployedAddresses = await safeDeploy(chainId, 2, 9)
+  fastify.log.info('deployment complete')
+  return { killed: false, node, deployedAddresses }
+}
+
+async function startDevEnv() {
+  try {
+    fastify.log.info('starting dev env')
+
+    if (devEnvChildProcess && !devEnvChildProcess.killed) {
+      fastify.log.info('dev env already started')
+      return false
+    }
+
+    if (fs.existsSync(paths.hardhatDeployedAddresses)) {
+      fastify.log.info(`removing ${paths.hardhatDeployedAddresses}`)
+      fs.rmSync(paths.hardhatDeployedAddresses)
+      fastify.log.info('file removed, starting dev env')
+    }
+
+    devEnvChildProcess = await runNodeAndDeploy()
+    fastify.log.info('dev env started')
+    return devEnvChildProcess.deployedAddresses
+  } catch (error) {
+    fastify.log.error(`starting dev env failed: ${error}`)
+    return false
+  }
+}
+
+function stopDevEnv() {
   if (devEnvChildProcess) {
-    devEnvChildProcess.kill()
     fastify.log.info('stopping dev env')
+    devEnvChildProcess = undefined
+    return true
   }
 
   fastify.log.info('dev env already stopped')
-})
+  return false
+}
 
-fastify.post('/create-snapshot', async () => {
+async function createSnapshot() {
   if (devEnvChildProcess) {
     fastify.log.info('creating snapshot')
-    devEnvChildProcess.send({ type: 'createSnapshot' })
+
+    try {
+      const snapshot = await takeSnapshot()
+      devEnvSnapshots.push(snapshot)
+      fastify.log.info(`snapshot ${snapshot.snapshotId} saved`)
+      return true
+    } catch (error) {
+      fastify.log.error(`creating snapshot failed: ${error}`)
+      return false
+    }
   } else {
     fastify.log.info('dev env not started')
+    return false
   }
-})
+}
 
-fastify.post('/restore-snapshot', async () => {
-  const snapshotId = devEnvSnapshots[0]?.snapshotId
-
-  if (devEnvChildProcess && snapshotId) {
-    fastify.log.info(`restoring snapshot ${snapshotId}`)
-    devEnvChildProcess.send({ type: 'restoreSnapshot', data: snapshotId })
-  } else {
-    fastify.log.info('dev env not started or invalid snapshot id')
-  }
-
-})
-
-fastify.get('/get-snapshots', async () => {
+async function restoreSnapshot() {
   if (devEnvChildProcess) {
-    fastify.log.info('fetching saved snapshot ids')
-    devEnvChildProcess.send({ type: 'getSnapshots' })
-    await(new Promise(resolve => setTimeout(resolve, 100)))
+    fastify.log.info('restoring snapshot')
+
+    try {
+      const snapshotId = devEnvSnapshots[0]?.snapshotId
+
+      if (snapshotId) {
+        fastify.log.info(`restoring snapshot ${snapshotId}`)
+        await devEnvSnapshots[0].restore()
+        fastify.log.info(`snapshot ${snapshotId} restored`)
+        return true
+      } else {
+        fastify.log.info('invalid snapshot id')
+        return false
+      }
+    } catch (error) {
+      fastify.log.error(`restoring Snapshot failed: ${error}`)
+      return false
+    }
+  } else {
+    fastify.log.info('dev env not started')
+    return false
+  }
+}
+
+function getSnapshotIds() {
+  if (devEnvChildProcess) {
     const snapshotIds = devEnvSnapshots.map(({ snapshotId }) => snapshotId)
     fastify.log.info(`saved snapshot ids: ${snapshotIds.join(', ')}`)
     return snapshotIds
   } else {
     fastify.log.info('dev env not started')
+    return false
   }
+}
+
+fastify.post('/start', startDevEnv)
+fastify.post('/stop', stopDevEnv)
+fastify.post('/create-snapshot', createSnapshot)
+fastify.post('/restore-snapshot', restoreSnapshot)
+fastify.get('/get-snapshots', getSnapshotIds)
+
+fastify.get('/test-get-balances', async () => {
+  const [sender, receiver] = await ethers.getSigners()
+  const senderETHBalance = ethers.utils.formatEther(await sender.getBalance())
+  const receiverETHBalance = ethers.utils.formatEther(await receiver.getBalance())
+  fastify.log.info(
+    `ETH balances: sender has ${senderETHBalance}, receiver has ${receiverETHBalance}`,
+  )
+  return { senderETHBalance, receiverETHBalance }
 })
 
-fastify.post('/test', async () => {
-  if (devEnvChildProcess) {
-    fastify.log.info('sending some txs against the dev env')
-    devEnvChildProcess.send({ type: 'test' })
-  } else {
-    fastify.log.info('dev env not started')
-  }
+fastify.post('/test-send-eth', async () => {
+  const [sender, receiver] = await ethers.getSigners()
+  fastify.log.info('Sending 1 ETH from sender to receiver')
+  const tx = await sender.sendTransaction({
+    to: receiver.address,
+    value: ethers.utils.parseEther('1'),
+  })
+  fastify.log.info(`Tx sent: ${tx.hash}`)
+  return { txHash: tx.hash }
 })
 
 fastify.ready().then(() => {
   fastify.listen({ port: 6667 })
 })
-
