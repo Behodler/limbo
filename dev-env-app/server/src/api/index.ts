634e4fc85da7a36f8fbc1d4d26e6d46f5656d693
michal.kloczko <michal.kloczko@tratif.com>
2023-02-03 14:18:27 +0100

    removed test API endpoints

--- dev-env-app/server/src/api/index.ts
+++ dev-env-app/server/src/api/index.ts
@@ -1,6 +1,3 @@
-import { ethers } from 'hardhat'
-import '@nomiclabs/hardhat-ethers'
-
 import { BehodlerDevEnvFastifyInstance } from '../types'
 import createSnapshot from './create-snapshot'
 import restoreSnapshot from './restore-snapshot'
@@ -12,27 +9,6 @@ export function api(fastify: BehodlerDevEnvFastifyInstance, _, done) {
   fastify.register(restoreSnapshot)
   fastify.register(getSnapshots)
   fastify.register(getDeploymentAddresses)
-
-  fastify.get('/test-get-balances', async () => {
-    const [sender, receiver] = await ethers.getSigners()
-    const senderETHBalance = ethers.utils.formatEther(await sender.getBalance())
-    const receiverETHBalance = ethers.utils.formatEther(await receiver.getBalance())
-    fastify.log.info(
-      `ETH balances: sender has ${senderETHBalance}, receiver has ${receiverETHBalance}`,
-    )
-    return { senderETHBalance, receiverETHBalance }
-  })
-
-  fastify.post('/test-send-eth', async () => {
-    const [sender, receiver] = await ethers.getSigners()
-    fastify.log.info('Sending 1 ETH from sender to receiver')
-    const tx = await sender.sendTransaction({
-      to: receiver.address,
-      value: ethers.utils.parseEther('1'),
-    })
-    fastify.log.info(`Tx sent: ${tx.hash}`)
-    return { txHash: tx.hash }
-  })
 
   done()
 }
