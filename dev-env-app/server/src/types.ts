76d485162ff74d37fb49aed6d2d7b629d7560445
michal.kloczko <michal.kloczko@tratif.com>
2023-03-09 10:56:27 +0100

    refactor

--- dev-env-app/server/src/types.ts
+++ dev-env-app/server/src/types.ts
@@ -1,6 +1,8 @@
 import { SnapshotRestorer } from '@nomicfoundation/hardhat-network-helpers'
 import { FastifyInstance } from 'fastify'
 
+import { recipeNames } from '../../../scripts/networks/common'
+
 export type BehodlerDevEnv = {
   active: boolean
   snapshots: SnapshotRestorer[]
@@ -22,4 +24,23 @@ export interface BehodlerDevEnvFastifyInstance extends FastifyInstance {
   startDevEnv?: StartDevEnv
   createInfoResponse?: CreateResponse
   createErrorResponse?: CreateResponse
+}
+
+export interface BehodlerDevEnvDeploymentConfig {
+  miningIntervalMs: number
+  autoMining: boolean
+  recipeName: recipeNames
+  confirmationsNumber: number
+  addressesJSONFilePath: string
+}
+
+export interface BehodlerDevEnvRuntimeConfig {
+  miningIntervalMs: number
+  autoMining: boolean
+}
+
+export interface BehodlerDevEnvNodeSettings {
+  noDeploy: boolean
+  silent: boolean
+  port: number
 }
